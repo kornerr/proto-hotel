0d6cf5f23f7b45e4873f7fba3f4b9a7035c00d75
Михаил Капелько <kornerr@gmail.com>
2024-08-22 22:36:29 +0300

    d

--- py/ht.py
+++ py/ht.py
@@ -77,3 +77,22 @@ def ht_resetSelectedGoto(
 
     c.recentField = "none"
     return c
+
+# Select comment (i.e., make it visible)
+#
+# Conditions:
+# 1. Item with an associated comment has just been selected
+@cld_by_value
+def ht_selectComment(
+    c: ht_Context
+) -> ht_Context:
+    if (
+        c.recentField == "selectedItem" and
+        c.selectedItem is not None
+    ):
+        c.selectedComment = ht_aux_itemComment(c.comments, c.selectedItem)
+        c.recentField = "selectedComment"
+        return c
+
+    c.recentField = "none"
+    return c
